09e0b65575eb876bfea2b7df4ebdf265b56cdfd4
Nikolaos Kantartzopoulos <95440122+NikolaosKantartzopoulos@users.noreply.github.com>
2022-09-12 12:12:36 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,18 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/i,
-				include: path.resolve(__dirname, "src"),
-				use: ["style-loader", "css-loader", "postcss-loader"],
+			test: /\.css$/i,
+			include: path.resolve(__dirname, "src"),
+			use: ["style-loader", "css-loader", "postcss-loader"],
 			},
+			{
+			test: /\.(png|svg|jpg|jpeg|gif)$/i,
+			type: 'asset/resource',
+		        },
+			{
+			test: /\.(woff|woff2|eot|ttf|otf)$/i,
+			type: 'asset/resource',
+		      },
 		],
 	},
 };
