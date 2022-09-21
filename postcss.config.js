ba58261bb4f19e7685fd18a0c7953b30ffe41c86
Nikolaos Kantartzopoulos <95440122+NikolaosKantartzopoulos@users.noreply.github.com>
2022-09-22 00:09:34 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,3 @@ const tailwindcss = require("tailwindcss");
 module.exports = {
 	plugins: ["postcss-preset-env", tailwindcss],
 };
-
-// https://github.com/postcss/postcss
-// module.exports = {
-//   plugins: [
-//     require('autoprefixer'),
-//     require('postcss-nested')
-//   ]
-// }
